3246be21576ec2b20c8ffa1988acdd0aa3ee516f
Olnipa <107411395+Olnipa@users.noreply.github.com>
2022-08-12 22:54:55 +0700

    Добавьте файлы проекта.

---
+++ bossDamage/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace bossDamage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int minBossDamage = 10;
+            int maxBossDamage = 50;
+            int bossDamage = random.Next(minBossDamage, maxBossDamage);
+
+            int minCriticalChance = 1;
+            int maxCriticalChance = 101;
+            int criticalChance = 50;
+            int criticalCoefficient = 3;
+
+            if (random.Next(minCriticalChance, maxCriticalChance) > criticalChance)
+            {
+                bossDamage *= criticalCoefficient;
+            }            
+        }
+    }
+}
